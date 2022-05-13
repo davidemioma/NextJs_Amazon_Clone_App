4b0b5e9ae9be1c31b93662b75986994fef1ce287
davidemioma <davidemiomauche@gmail.com>
2022-05-13 17:14:36 +0100

    page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Banner from "../components/banner/Banner";
 import ProductsFeed from "../components/products/ProductsFeed";
 import axios from "axios";
 
-const Home = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://fakestoreapi.com/products")
-      .then((res) => setProducts(res.data))
-      .catch((err) => alert(err.message));
-  }, []);
-
+const Home = ({ products }) => {
   return (
     <div className="container">
       <Banner />
@@ -20,6 +11,18 @@ const Home = () => {
       <ProductsFeed products={products} />
     </div>
   );
+};
+
+export const getServerSideProps = async (context) => {
+  const products = await axios
+    .get("https://fakestoreapi.com/products")
+    .then((res) => res.data);
+
+  return {
+    props: {
+      products,
+    },
+  };
 };
 
 export default Home;
